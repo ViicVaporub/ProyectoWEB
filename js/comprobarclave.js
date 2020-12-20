e7e42cb87b54b24e9617ce4389d473355880a413
Odracir2435 <ricardomunoz2435@gmail.com>
2020-12-20 03:04:00 -0600

    hola

---
+++ js/comprobarclave.js
@@ -0,0 +1,11 @@
+function comprobarClave(){
+    clave1 = document.clave.clave1.value
+    clave2 = document.clave.clave2.value
+
+if(clave1 == clave2){
+    alert("Las dos claves son iguales")
+}else{
+    alert("No tiene coinciden las claves")
+}
+
+}
