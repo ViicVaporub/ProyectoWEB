2f208315c0332bbc33817e26501ad33482d079ba
Odracir2435 <ricardomunoz2435@gmail.com>
2020-12-19 20:57:49 -0600

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,9 +1,9 @@
 <?php
  $destino= "s.reynoso.hdz@gmail.com";
- $name    = $_POST[name];
- $email   = $_POST[email];
- $subject = $_POST[subject]; 
- $message = $_POST[message];
+ $name    = $_POST['name'];
+ $email   = $_POST['email'];
+ $subject = $_POST['subject']; 
+ $message = $_POST['message'];
  $contenido = "Nombre: " . $nombre . "\nCorreo: " . $email . "\nMensaje: " . $message;
  mail($destino,$subject,$contenido);
 ?>
