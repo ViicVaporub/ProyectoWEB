c88946df122601981b33de5a1637e78a007b101b
JosefatMH <moralesisaac.mtto284@gmail.com>
2020-12-21 00:49:20 -0600

    comprobar password

--- FormularioRegistro.php
+++ FormularioRegistro.php
@@ -58,22 +58,18 @@
                             <h3 id="n-color">Registro</h3>
                             <button type="button" class="close font-weight-light" data-dismiss="modal" aria-hidden="true">×</button>
                         </div>
-                        <form class="form" role="form" action="registro.php" method="post" name="clave">
+                        <form class="form" id="form" role="form" action="registro.php" method="post" name="clave">
                             <div class="modal-body">
 
-                                <p>Nombre: <input type="text" name="login" style="margin-left: 14px;"> </p>
-                                <p>Contraseña: <input type="password" name="contra" style="margin-left: 10px;"> </p>
-                                <p>Confirmacion: <input type="password" name="contra" style="margin-left: 10px;" placeholder="Confirmacion de contraseña"> </p>
-                                <p>Correo: <input type="text" name="correo" style="margin-left: 23px;"> </p>
-                                
-                                <p>Contraseña: <input type="password" name="contra1" style="margin-left: 10px;"> </p>
-                                <p>Verificar Contraseña: <input type="password" name="contra2" style="margin-left: 10px;"> </p>
+                                <p>Nombre: <input type="text" name="login" style="margin-left: 14px;"> </p>                                
+                                <p>Contraseña: <input  type="password" name="contra1" id="contra1" style="margin-left: 10px;"> </p>
+                                <p>Verificar Contraseña: <input type="password" name="contra2" id="contra2" style="margin-left: 10px;"> </p>
                                 <p>Correo: <input type="email" name="correo" style="margin-left: 23px;"> </p>
 
                             </div>
                             <div class="modal-footer">
                                 <div class="form-group">
-                                    <button type="submit" class="btn btn-primary btn-block">Enviar</button>
+                                    <button type="submit" class="btn btn-primary btn-block" onclick="comprobarClave()">Enviar</button>
                                 </div>
                             </div>
                         </form>
--- js/comprobarclave.js
+++ js/comprobarclave.js
@@ -1,11 +1,15 @@
-function comprobarClave(){
-    clave1 = document.clave.clave1.value
-    clave2 = document.clave.clave2.value
+const clave2=document.getElementById('contra2');
+const clave1=document.getElementById('contra1');
 
-if(clave1 == clave2){
-    alert("Las dos claves son iguales")
+
+function comprobarClave(){
+    const clave1val = clave1.nodeValue.trim();
+    const clave2val = clave2.nodeValue.trim();
+
+if(clave1val==clave2val){
+    alert("Las dos claves son iguales");
 }else{
-    alert("No tiene coinciden las claves")
+    alert("las claves no coinciden");
 }
 
 }
